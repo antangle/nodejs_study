301612eeb231d0653867740926a895b931d5056f
= <zanaris@naver.com>
2020-07-12 16:37:27 +0900

    a

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
+const cors = require('cors');
 const app = express();
 
 //view engine
 //app.set('views', path.join(__dirname, 'views'));
 //app.set('view engine', 'pug');
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
