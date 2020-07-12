67e3ba3806450275254184c46e2553fa83855173
= <zanaris@naver.com>
2020-07-12 19:44:15 +0900

    a

--- query/createOrDrop.js
+++ query/createOrDrop.js
@@ -1,8 +1,6 @@
 const Pool = require('./pool');
 const pool = Pool.pool;
 
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit:'50mb', extended: false }));
 
 pool.on('connect', () => {
   console.log('connected to the db');
