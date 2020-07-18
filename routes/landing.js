60259ce1379de87f57b09f46ebdc60e89e8c4631
= <zanaris@naver.com>
2020-07-18 17:06:18 +0900

    Signed-off-by: = <zanaris@naver.com>

--- routes/landing.js
+++ routes/landing.js
@@ -46,6 +46,7 @@ router.post('/', async (req, res)=>{
         RETURNING status
         `;
         var {rows} = await query(querytext, [name, phone_num, email, is_auth]);
+        console.logs(rows);
         var isoverlap = rows[0].status;
         if(isoverlap == null){
             var result = {status: 'success'};
